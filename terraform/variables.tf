3d77e1cdd40e452fc8f994fba88f530d8ce63dd0
Rekiles <byllit@gmail.com>
2026-06-10 21:44:53 +0700

    Fix: infra orchestration, ssh integration and app startup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,3 @@
-variable "access_key" {
-  description = "Yandex Cloud access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key" {
-  description = "Yandex Cloud secret key"
-  type        = string
-  sensitive   = true
-}
 
 variable "cloud_id" {
   description = "Yandex Cloud ID"
